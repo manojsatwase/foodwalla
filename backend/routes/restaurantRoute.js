7dc541b1ba1f8bc185117733c27908f676c66651
manoj_s <apple@manojsatwase.local>
2024-03-27 22:59:19 +0530

    add route for findNearestRestaurant

--- backend/routes/restaurantRoute.js
+++ backend/routes/restaurantRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { createRestaurant, getAllRestaurants } = require('../controllers/restaurantController');
+const { findNearestRestaurant } = require('../controllers/findNearestRestaurant');
+const { isAuthenticated, restrictToAdmin } = require('../utils/auth');
 
 const router = express();
 
-router.route("/restaurants/create").post(createRestaurant);
-router.route("/restaurants").get(getAllRestaurants)
+router.route("/restaurants/create").post(isAuthenticated,restrictToAdmin, createRestaurant);
+router.route("/restaurants").get(getAllRestaurants);
+router.route("/nearest-restaurants").get(findNearestRestaurant);
 
 module.exports = router;
