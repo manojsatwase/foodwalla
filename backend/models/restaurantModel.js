51d45e032df898429dabe6412c03e2d2144649b0
manoj_s <apple@manojsatwase.local>
2024-03-27 22:58:15 +0530

    add restaurant schema key business_email key ,logo ,pincode

--- backend/models/restaurantModel.js
+++ backend/models/restaurantModel.js
@@ -8,12 +8,29 @@ const restaurantSchema = new mongoose.Schema({
     minlength: [2, 'Name should be at least 2 characters long'],
     maxlength: [50, 'Name should not exceed 50 characters'],
   },
+  business_email: {
+    type: String,
+    unique: true,
+    required: [true, "Please enter Email"],
+    trim: true,
+    lowercase: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+  },
+  logo:{
+    public_id: String,
+    url: String,
+  },
   address: {
     type: String,
     required: [true, 'Please enter the address'],
     trim: true, // Trim whitespace from input
     minlength: [5, 'Address should be at least 5 characters long'],
     maxlength: [100, 'Address should not exceed 100 characters'],
+  },
+  pincode:{
+    type:Number,
+    required: [true, 'Please enter the pincode'],
+    unique: true,
   },
   location: {
     type: {
