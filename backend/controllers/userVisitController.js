2bf725b58a220b37984cc1144c128ae46b17bd9e
manoj_s <apple@manojsatwase.local>
2024-03-27 23:01:40 +0530

    modify user visit controller

--- backend/controllers/userVisitController.js
+++ backend/controllers/userVisitController.js
@@ -1,60 +1,67 @@
 const UserVisitInfo = require('../models/userVisitModel');
 const Restaurant = require('../models/restaurantModel');
 const asyncHandler = require('express-async-handler');
-const { maxDistanceRadians } = require('../utils/constant');
+const {EARTH_RADIUS_METERS } = require('../utils/constant');
 
 exports.createUserVisitInfo = asyncHandler(async (req, res) => {
     try {
-        const { name, age, gender, location } = req.body;
+        const { name, age, gender,distance = 500, location } = req.body;
         // Assuming location is provided as an object with 'longitude' and 'latitude' properties
+        const longitude = parseFloat(location.longitude);
+        const latitude = parseFloat(location.latitude)
+        
         const userLocation = {
             type: 'Point',
-            coordinates: [location.longitude, location.latitude],
+            coordinates: [longitude,latitude],
         };
        
         // Find the nearby restaurant within 500m of the user's location
-        // Use maxDistanceRadians in Mongoose query
-            const nearbyRestaurant = await Restaurant.findOne({
-                location: {
-                    $near: {
-                        $geometry: {
-                            type: 'Point',
-                            coordinates: userLocation.coordinates,
-                        },
-                        $maxDistance: Number(maxDistanceRadians), // Convert to number
-                    },
+        const nearbyRestaurant = await Restaurant.aggregate([
+            {
+                $geoNear: {
+                near: {
+                    type: 'Point',
+                    coordinates: userLocation.coordinates // Note the order: [longitude, latitude]
                 },
-            });
-        
-        if (nearbyRestaurant) {
+                key:"location",
+                maxDistance: parseFloat(distance * EARTH_RADIUS_METERS), // Convert distance meters to radians using accurate Earth's radius
+                distanceField: 'dist.calculated',  
+                spherical: true
+                }
+            },
+        ]);
+ 
+        if (nearbyRestaurant?.length) {
             // Check if the user has already visited this restaurant
-            const existingVisit = await UserVisitInfo.findOne({
-                name,
-                restaurant: nearbyRestaurant._id,
-            });
-          
-            if (existingVisit) {
+            const existingVisit = await UserVisitInfo.find({name});
+            if (existingVisit?.length) {
                 // User has already visited this restaurant, return the details
                 return res.status(200).json({
                     success: true,
                     message: 'You have already visited this restaurant.',
-                    restaurant: nearbyRestaurant,
-                    visitInfo: existingVisit,
+                    restaurants: nearbyRestaurant,
                 });
             }else{
                // Save the new user visit information with restaurant reference
-            const userVisitInfo = await UserVisitInfo.create({
+               const restaurantId = [];
+               for(let restaurant of nearbyRestaurant){
+                restaurantId.push(restaurant._id);
+               }
+               
+                await UserVisitInfo.create({
                 name,
                 age,
                 gender,
+                distance,
                 location: userLocation,
-                restaurant: nearbyRestaurant._id,
-            });
-            return res.status(201).json({
-                success: true,
-                message: 'Thank you for visiting the restaurant.',
-                userVisitInfo,
-            });
+                restaurantId,
+             })
+            
+                return res.status(201).json({
+                    success: true,
+                    message: 'Thank you for visiting the restaurant.',
+                    restaurants:nearbyRestaurant 
+                });
            }       
         }
         return res.status(200).json({
