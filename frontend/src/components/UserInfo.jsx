cc48af1a0b00066f9b0f0efc1bf99cb7e6975b6c
manoj_s <apple@manojsatwase.local>
2024-03-28 12:41:26 +0530

    add restaurant component for show restaurant

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import UserInfoForm from '../forms/UserInfoForm';
 import { useSelector } from 'react-redux';
 import MessageComponent from './MessageComponent';
+import Restaurant from './Restaurant';
 
 const UserInfo = () => {
   const message = useSelector(state=>state.userVisitInfo?.message);
+  const restaurants = useSelector(state=>state.userVisitInfo?.visitInfo?.restaurants);
+
   return (
     <div className="userInfo">
-      {!message ? <UserInfoForm /> : <MessageComponent message={message} />}
+      {!restaurants?.length ? <UserInfoForm /> : (
+        <>
+          <MessageComponent message={message} />
+          <Restaurant restaurants={restaurants} />
+        </>
+      )}
     </div>
   );
 };
