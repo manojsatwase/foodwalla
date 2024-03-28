98365317ac5d49897db025337ee5c935a01b0bfd
manoj_s <apple@manojsatwase.local>
2024-03-28 06:41:26 +0530

    add more restuarant fields

--- backend/models/restaurantModel.js
+++ backend/models/restaurantModel.js
@@ -11,27 +11,56 @@ const restaurantSchema = new mongoose.Schema({
   business_email: {
     type: String,
     unique: true,
-    required: [true, "Please enter Email"],
+    required: [true, 'Please enter Email'],
     trim: true,
     lowercase: true,
     match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
   },
-  logo:{
+  logo: {
     public_id: String,
     url: String,
+  },
+  description: {
+    type: String,
+    required: [true, 'Please enter a description'],
+    trim: true,
+    minlength: [10, 'Description should be at least 10 characters long'],
+    maxlength: [500, 'Description should not exceed 500 characters'],
+  },
+  rating: {
+    type: Number,
+    default: 0, // Default rating value
+    min: 0,
+    max: 5, // Assuming a 5-star rating system
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please enter the average price for two'],
+    min: 0, // Assuming price cannot be negative
   },
   address: {
     type: String,
     required: [true, 'Please enter the address'],
-    trim: true, // Trim whitespace from input
+    trim: true,
     minlength: [5, 'Address should be at least 5 characters long'],
     maxlength: [100, 'Address should not exceed 100 characters'],
   },
-  pincode:{
-    type:Number,
+  pincode: {
+    type: Number,
     required: [true, 'Please enter the pincode'],
     unique: true,
   },
+  openAt: {
+    type: String,
+    required: [true, 'Please enter the opening time'],
+    validate: {
+      validator: function(value) {
+        // Regular expression to validate time format in 24-hour format
+        return /^([01]\d|2[0-3]):([0-5]\d)$/.test(value);
+      },
+      message: 'Opening time should be in 24-hour format (e.g., "07:00")',
+    },
+  },  
   location: {
     type: {
       type: String,
@@ -40,8 +69,8 @@ const restaurantSchema = new mongoose.Schema({
     },
     coordinates: {
       type: [Number],
-      default: [0, 0], // Default coordinates
-      index: '2dsphere', 
+      default: [0, 0],
+      index: '2dsphere',
     },
   },
 });
@@ -55,5 +84,3 @@ restaurantSchema.path('location.type').validate(function (value) {
 
 
 module.exports = mongoose.model('Restaurant', restaurantSchema);
-
-
