46fc6777e928c17d59965eead5f30ea967efcff2
manoj_s <apple@manojsatwase.local>
2024-03-29 09:50:03 +0530

    refactor code and design card

--- frontend/src/components/RestaurantCard.jsx
+++ frontend/src/components/RestaurantCard.jsx
@@ -1,35 +1,49 @@
 import React from 'react';
 import { Card, Badge, Image } from 'react-bootstrap';
-import logoUrl from '../assets/img/banner.png';
-import '../utils/common';
+import { Link } from 'react-router-dom';
 
-const RestaurantCard = ({ name, rating, price, address, description, openAt, pincode, dist, onClick }) => {
-  const distance = Math.floor(dist?.calculated / 1000); // Calculate distance in kilometers
+import logoUrl from '../assets/img/banner.png';
+import star from '../assets/star.png';
+
+import {convertMeterToKM} from '../utils/common';
+
+const RestaurantCard = ({ _id, name, rating, price, address, description, openAt, pincode, dist }) => {
+  const distance = convertMeterToKM(dist) // Calculate distance in kilometers
+
   return (
-    <Card className='restaurantCard' style={{ maxWidth: '30%' }}>
-      <Card.Header className='p-0'>
-        <Card.Link href={logoUrl}>
-          <Image src={logoUrl} alt="Restaurant Logo" fluid style={{ width: '100%' }} />
-        </Card.Link>
-      </Card.Header>
-      <Card.Body>
-        <div className="d-flex justify-content-between align-items-center pb-3" style={{fontSize:"1.1rem"}}>
-          <Card.Title>{name?.capitalize()}</Card.Title>
-          <Badge bg="white" className="text-success pb-3">Rating:{rating}⭐️</Badge>
-        </div>
-        <Card.Subtitle className="mb-2 text-muted">
-          <Badge bg="warning" className="text-white">Price: ${price}</Badge>{' '}
-          <Badge bg="warning" className="text-white">Distance: {distance} KM</Badge>
-        </Card.Subtitle>
-        <Card.Text>{description.capitalize()}</Card.Text>
-        <Card.Text>
-          <strong>Address:</strong> {address.capitalize()}, {pincode}
-        </Card.Text>
-        <Card.Text>
-          <strong>Opening Time:</strong> {openAt}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <Link to={`/restaurant/${_id}`} className='pb-5' style={{ textDecoration: 'none' }}>
+      <Card style={{ borderRadius: '1rem' }}>
+        <Card.Header className='p-0'>
+          <div className='thumnail'>
+            <Image src={logoUrl} alt="Restaurant Logo" fluid style={{ width: '100%' }} />
+          </div>
+        </Card.Header>
+        <Card.Body>
+          <div className="d-flex justify-content-between align-items-center pb-3" style={{ fontSize: "1.1rem" }}>
+            <Card.Title>{name?.capitalize()}</Card.Title>
+            <Badge bg="white" className="text-success pb-3">
+              Rating:{rating}
+              <img
+                src={star}
+                alt='star'
+                className='star_img'
+              />
+            </Badge>
+          </div>
+          <Card.Subtitle className="mb-2 text-muted">
+            <Badge bg="warning" className="text-white">Price: ${price}</Badge>{' '}
+            <Badge bg="warning" className="text-white">Distance: {distance} KM</Badge>
+          </Card.Subtitle>
+          <Card.Text>{description.capitalize().truncateWithDots()}</Card.Text>
+          <Card.Text>
+            <strong>Address:</strong> {address.capitalize()}, {pincode}
+          </Card.Text>
+          <Card.Text>
+            <strong>Opening Time:</strong> {openAt}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 
