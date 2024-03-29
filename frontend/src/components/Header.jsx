6977f140e3e00a30e818b7029098e031cb9a0a74
manoj_s <apple@manojsatwase.local>
2024-03-29 10:36:51 +0530

    sticky header to top and remove pl class

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from 'react-router-dom'; // Import NavLink for active styling
+import { Link, NavLink } from 'react-router-dom';
 import { Container, Form, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
@@ -6,22 +6,22 @@ import { useState } from 'react';
 import Logo from '../assets/img/logo.png';
 import UserPic from '../assets/userpic.png';
 
-import 'bootstrap-icons/font/bootstrap-icons.css'; 
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Header = () => {
   const [searchText, setSearchText] = useState('');
   const dispatch = useDispatch();
 
   const Logout = () => {
-      //dispatch(logoutUserAPI());
+    //dispatch(logoutUserAPI());
   }
 
-  const onChangeHandler = ({target:{value}}) => {
+  const onChangeHandler = ({ target: { value } }) => {
     setSearchText(value)
   }
 
   return (
-    <Navbar expand="lg" bg="light" variant='light'>
+    <Navbar expand="lg" bg="light" variant='light' className="sticky-top">
       <Container>
         <Navbar.Brand>
           <Link to="/" onClick={() => setTab("/")}>
@@ -30,11 +30,11 @@ const Header = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         {/* Conditional rendering based on user authentication */}
-          
+
         {true ? (
           <Navbar.Collapse id="navbarScroll">
             <Nav className="m-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-              <Form className="d-flex" style={{width:"250px"}}>
+              <Form className="d-flex" style={{ width: "250px" }}>
                 <Form.Control
                   type="search"
                   placeholder="Search"
@@ -46,10 +46,10 @@ const Header = () => {
               </Form>
             </Nav>
             {/* Cart icon and link */}
-            <NavLink to="/" className='pl-3 nav-link'>
+            <NavLink to="/" className='nav-link'>
               Home
             </NavLink>
-            <NavLink to="/menu" className='pl-3 nav-link'>
+            <NavLink to="/menu" className='nav-link'>
               Menu
             </NavLink>
             <NavLink to="/cart" className="nav-link cart-link pl-2">
@@ -62,7 +62,7 @@ const Header = () => {
               false && (
                 <>
                   <img src={UserPic} className='userProfile' lazy="loading" alt='user' />
-                  <NavDropdown title={"manoj satwase"} id="navbarScrollingDropdown"> 
+                  <NavDropdown title={"manoj satwase"} id="navbarScrollingDropdown">
                     <NavLink className='p-4' to="/myprofile">
                       My Profile
                     </NavLink>
