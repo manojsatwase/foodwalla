2db82682fda4ad1577ad0ba503951917ce015921
manoj_s <apple@manojsatwase.local>
2024-03-29 09:55:33 +0530

    add truncateWithDots for restaurant description and convertMeterToKM

--- frontend/src/utils/common.jsx
+++ frontend/src/utils/common.jsx
@@ -1,3 +1,12 @@
 String.prototype.capitalize = function () {
     return this.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
 };
+
+String.prototype.truncateWithDots = function(maxLength=100) {
+    if (this.length > maxLength) {
+      return this.slice(0, maxLength) + '...';
+    }
+    return this.toString();
+  };
+
+export const convertMeterToKM = (dist) => Math.floor(dist?.calculated / 1000);
