efd862797c9a9c10ded3b22550aa55cff33efd49
manoj_s <apple@manojsatwase.local>
2024-03-29 09:48:10 +0530

    refactor code

--- frontend/src/components/MessageComponent.jsx
+++ frontend/src/components/MessageComponent.jsx
@@ -1,17 +1,7 @@
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { clearErrors, clearMessage } from '../redux/slices/userVisitInfoSlice';
-
 const MessageComponent = ({ message }) => {
-  const dispatch = useDispatch();
-  const handleClearMessage = () => {
-    dispatch(clearMessage());
-    dispatch(clearErrors());
-  };
   return (
-    <div className="messageContainer">
+    <div className="messageContainer pt-3">
       <p className="messageText">{message}</p>
-      <Link className="messageLink" onClick={handleClearMessage} to="/">Back To Home</Link>
     </div>
   );
 };
